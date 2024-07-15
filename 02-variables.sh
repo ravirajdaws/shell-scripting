c21335036304d567d7a51567aea90dd7bf8700c4
ravirajdaws <raviraj.devops78@gmail.com>
2024-07-15 20:52:20 +0530

    variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Ramesh : Hello Suresh, how are you ?"
-echo "suresh : hi Ramesh , I am fine how are you"?
+echo "suresh : hi Ramesh , I am fine how are you?"
 echo "Ramesh : I am fine too , how is your work ?"
 echo "Suresh : Not bad , I am thinking to upgrade to devops"
