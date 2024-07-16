adc68fa405e7066a22076ccbe8cfb9118c3b14d2
ravirajdaws <raviraj.devops78@gmail.com>
2024-07-16 23:29:52 +0530

    declaring variable

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#declaring variable
+PERSON1=Ramesh #no space between = and value
+PERSON2=Suresh
+
+#reffering variable
+echo "$PERSON1 : Hello $PERSON2, how are you ?"
+echo "$PERSON2 : hi $PERSON1 , I am fine how are you?"
+echo "$PERSON1 : I am fine too , how is your work ?"
+echo "$PERSON2 : Not bad , I am thinking to upgrade to devops"
