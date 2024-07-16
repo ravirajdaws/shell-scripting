48b3b15d7c0cba9c6cd96ba95e1b561d4f62b5d1
ravirajdaws <raviraj.devops78@gmail.com>
2024-07-16 23:50:50 +0530

    declaring variable values

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# declaring variable
+PERSON1=$1
+PERSON2=$2 #$1 AND $2 are the Arguments /parameters
+
+#referring variable
+echo "$PERSON1 : Hello $PERSON2, how are you ?"
+echo "$PERSON2 : hi $PERSON1 , I am fine how are you?"
+echo "$PERSON1 : I am fine too , how is your work ?"
+echo "$PERSON2 : Not bad , I am thinking to upgrade to devops"
