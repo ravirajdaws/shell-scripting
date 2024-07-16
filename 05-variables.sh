85a8d766c302019ea9aca6c80e5fffcd20013c50
ravirajdaws <raviraj.devops78@gmail.com>
2024-07-17 00:08:54 +0530

    user credentials

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "please enter the username :"
+echo "please enter the username:"
 read USERNAME # the username is the variable
 echo "please enter the password:"
 read PASSWORD # the password is the variable 
 
-echo "username is :$USERNAME password is :$PASSWORD"
+echo "username is: $USERNAME password is: $PASSWORD"
