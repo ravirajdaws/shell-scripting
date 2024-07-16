6dce7ca18b3cf8e91fcbfdf8b598335662cd4d38
ravirajdaws <raviraj.devops78@gmail.com>
2024-07-17 02:45:51 +0530

    using array

---
+++ 06.array.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+movies=("RRR" "DJtillu" "Pokiri")
+
+#size of the above array is 3
+
+#index are 0,1,2
+
+#list always starts with Zero (0)
+
+echo "first movie is:${movies[0]}"
+
+echo "second movie is:${movies[1]}"
+
+echo "list of all movies is:${movies[2]}"
+
