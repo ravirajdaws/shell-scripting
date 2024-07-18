3b0abd9bfb1ea3702381c6cc5b9a99b44bed9662
ravirajdaws <raviraj.devops78@gmail.com>
2024-07-18 20:41:47 +0530

    installation

--- 10-installation.sh
+++ 10-installation.sh
@@ -10,11 +10,20 @@ else
 echo "you are super user"
 fi 
 
-#dnf install mysql -y
-#if [ $? -ne 0 ]
-#then 
-#echo "installation of mysql ......failure"
-#exit 1
-#else 
-#echo "installation of mysql ........sucess"
-#fi
+dnf install mysql -y
+if [ $? -ne 0 ]  # '$?' which means the exit status of previous command
+then 
+echo "installation of mysql ..........failure"
+exit 1 # manually need to insert the exit status 1 as shell program will continue to next line of code with out exit
+else 
+echo "installation of mysql..........success"
+fi 
+
+dnf installation git -y
+if [ $? -ne 0 ]
+then
+echo "installation of git ...........failure"
+exit 1
+else
+echo "installation of git .................success"
+fi
