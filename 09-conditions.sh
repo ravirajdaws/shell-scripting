0e5bac929e6081bf0fee032643f6d307b8b8cdcb
ravirajdaws <raviraj.devops78@gmail.com>
2024-07-18 16:59:20 +0530

    conditiions

---
+++ 09-conditions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [$number -gt 10]
+
+then 
+echo "Given number $NUMBER is greater than 10"
+else
+echo "Given number $NUMBER is lesser than 10"
+
+if [$number -eq 10]
+
+then 
+echo "Given number $NUMBER is equal to 10"
+else 
+echo "Given number $NUMBER is not equal to 10"
+
+fi 
