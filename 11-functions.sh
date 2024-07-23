bf7be9c035f5b4a1a2bc09d394666378d1313251
ravirajdaws <raviraj.devops78@gmail.com>
2024-07-23 19:35:20 +0530

    functions with logfile and timestamp

--- 11-functions.sh
+++ 11-functions.sh
@@ -26,6 +26,6 @@ VALIDATE(){
  dnf install mysql -y &>>$LOGFILE
   VALIDATE $? "installing mysql" 
 
- dnf install git -y &>>LOGFILE
+ dnf install git -y &>>$LOGFILE
  VALIDATE $? "installing git"
 
