8c8b2b49b852e0070a8a6b937474b3803b9d6662
ravirajdaws <raviraj.devops78@gmail.com>
2024-07-23 18:52:03 +0530

    functions with logfile and timestamp

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE(){
  if [ $1 -ne 0 ]
@@ -20,9 +23,9 @@ VALIDATE(){
   echo "you are super user"
  fi 
 
- dnf install mysql -y
-  VALIDATE $? "installing mysql"
+ dnf install mysql -y &>>$LOGFILE
+  VALIDATE $? "installing mysql" 
 
- dnf install git -y
+ dnf install git -y &>>LOGFILE
  VALIDATE $? "installing git"
 
