8653d8fb8a53ceff9967a7a98e8f4732059fecd1
ravirajdaws <raviraj.devops78@gmail.com>
2024-08-08 21:25:13 +0530

    calling other scripti

---
+++ 15-current-script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+COURSE="Devops from the current script"
+
+echo "before calling other script , course:$COURSE"
+echo "process ID of current shell script:$$"
+
+#./16-other-script.sh
+source ./16-other-script.sh
+
+echo "After calling other script, course:$COURSE"
+echo "process ID of the current shell script:$$"
+
